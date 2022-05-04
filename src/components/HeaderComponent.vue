5d46a6cbb932dbefeccd464b2025eedcf8dfae7a
nicolas-dp <wellcomenicolas@gmail.com>
2022-05-04 17:20:41 +0200

    Add border bottom header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,102 +1,107 @@
 <template>
   <header>
-      <div class="container">
-        <nav class="logo_navabar">
-          <div class="logo">
-            <img style="height: 70px;" src="@/assets/img/dc-logo.png" alt="..." />
+    <div class="container">
+      <nav class="logo_navabar">
+        <div class="logo">
+          <img style="height: 70px" src="@/assets/img/dc-logo.png" alt="..." />
+        </div>
+        <div class="menu">
+          <div class="border_bottom" v-for="item in menu" :key="item.id">
+            <a :href="item.href">{{ item.link }}</a>
           </div>
-          <div class="menu">
-            <a :href="item.href" v-for="item in menu" :key="item.id">{{item.link}}</a>
-          </div>
-        </nav>
-      </div>
-    </header>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'SiteHeader',
-  data(){
-    return{
+  name: "SiteHeader",
+  data() {
+    return {
       menu: [
         {
           id: 1,
-          href: '#',
-          link: 'characters'
+          href: "#",
+          link: "characters",
         },
         {
           id: 2,
-          href: '#',
-          link: 'comics'
+          href: "#",
+          link: "comics",
         },
         {
           id: 3,
-          href: '#',
-          link: 'movies'
+          href: "#",
+          link: "movies",
         },
         {
           id: 4,
-          href: '#',
-          link: 'tv'
+          href: "#",
+          link: "tv",
         },
         {
           id: 5,
-          href: '#',
-          link: 'games'
+          href: "#",
+          link: "games",
         },
         {
           id: 6,
-          href: '#',
-          link: 'collectibles'
+          href: "#",
+          link: "collectibles",
         },
         {
           id: 7,
-          href: '#',
-          link: 'videos'
+          href: "#",
+          link: "videos",
         },
         {
           id: 8,
-          href: '#',
-          link: 'fans'
+          href: "#",
+          link: "fans",
         },
         {
           id: 9,
-          href: '#',
-          link: 'news'
+          href: "#",
+          link: "news",
         },
         {
           id: 10,
-          href: '#',
-          link: 'shop'
-        }
-
-      ]
-    }
-  }
-}
+          href: "#",
+          link: "shop",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scope>
-  /* Site Header */
+/* Site Header */
 
 .logo_navabar {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
+}
+.menu {
+  display: flex;
+  align-items: center;
+}
+
+.border_bottom {
+  padding: 1.5rem;
+  &:hover {
+    border-bottom: 4px solid $dc-primary;
+  }
 }
 
 a {
   text-decoration: none;
   color: $dc-main-bg;
-  padding: 0.5rem;
   text-transform: uppercase;
-
-  &:hover {
-    border-bottom: 3px solid $dc-primary;
-;
-  }
 }
 </style>
